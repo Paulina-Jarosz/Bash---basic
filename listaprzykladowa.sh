d5ceac0ba8998ab4a5a283931210cf2e89f08e6d
Paulina Jarosz <paulinajarosz23@gmail.com>
2019-11-17 18:14:43 +0100

    basic exercise

---
+++ listaprzykladowa.sh
@@ -0,0 +1,15 @@
+#1/usr/bin/env bash
+
+# lista przykladowa (wyswietla na ekranie ten komentarz, tez go uwzglednia w sortowaniu), czyli komenda sort < listaprzykladowa.sh
+
+jablko
+pomarancz
+gruszka
+banan
+truskawka
+porzeczka
+sliwka
+
+# komenda nl (number lines) numeruje linie i wyswietla na terminalu
+# komenda wc "word count" liczy ilosc lini i wyswietla tylko wynik
+# wyslalam wynik sortowania do pliku wyniklisty.sh za pomoca komend sort i >
